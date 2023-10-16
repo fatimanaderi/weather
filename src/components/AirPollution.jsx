379f4d483f2f505544dbb7eea894fb9bcc806d58
Fatima Naderi <fatimaanaderi@gmail.com>
2023-10-16 10:41:28 +0330

    change color progress

--- src/components/AirPollution.jsx
+++ src/components/AirPollution.jsx
@@ -1,5 +1,6 @@
 import useAirPollution from "../hooks/useAirPollution";
-
+import { TbBuildingFactory } from "react-icons/tb";
+import { FaTree } from "react-icons/fa";
 const AirPollution = () => {
   const { data, err, loading, airState } = useAirPollution();
   if (loading || err) return <>...</>;
@@ -7,7 +8,14 @@ const AirPollution = () => {
   return (
     <div className="my-4 mx-5">
       <div className={"circle" + " " + "per-" + stateNumber}>
-        <div className="inner">{airState[stateNumber]}</div>
+        <div className="inner">
+          {airState[stateNumber]}
+          {[1, 2, 3].includes(+stateNumber) ? (
+            <TbBuildingFactory className="ml-1" />
+          ) : (
+            <FaTree className="ml-1" />
+          )}
+        </div>
       </div>
       <div className="my-4 mt-5 glassmorphism h-fit rounded-2xl">
         {data &&
