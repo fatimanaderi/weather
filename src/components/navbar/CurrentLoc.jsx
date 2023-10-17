6386fa446efc0ed97374f2464f824c7498b95d4d
Fatima Naderi <fatimaanaderi@gmail.com>
2023-10-17 17:36:27 +0330

    add margin to currentLoc

--- src/components/navbar/CurrentLoc.jsx
+++ src/components/navbar/CurrentLoc.jsx
@@ -1,10 +1,10 @@
 const CurrentLoc = ({ city, country, date }) => {
   return (
     <div className="w-full sm:w-1/4 my-5 ">
-      <h1 className="text-2xl font-bold mr-2">
+      <h1 className="text-2xl font-bold mr-2 lg:ml-10">
         {city}, {country}
       </h1>
-      <h2>{date}</h2>
+      <h2 className="lg:ml-10">{date}</h2>
     </div>
   );
 };
